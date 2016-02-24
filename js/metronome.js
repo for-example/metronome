a2f9c8abfddccca82f5758a4848561a4c96e937c
for-example <for-example@users.noreply.github.com>
2016-02-24 18:41:11 -0500

    dynamic html

--- js/metronome.js
+++ js/metronome.js
@@ -2,6 +2,7 @@ initModule = function (  ) {
 
 /* state variables */
   var playing = false;
+  var num_measures = 16;
 
 /* event handlers */  
   onClick = function(e) {
@@ -15,12 +16,23 @@ initModule = function (  ) {
 
 /* setup */  
   setup = function() {
-   
+    
+    // html structures
+    html = '<tr>';
+    for (var i = 0; i < num_measures; i++) {
+      html += '<td id="m'+ i +'"></td>';
+    } 
+    html += '</tr>';
+    $("#metronome").html(html);
+    // end html structures
+ 
+    // highlighting   
     // button
     $("#play").val($("<div>").html("&#9654;").text());
     
     // metronome
     $("#m0").addClass("highlite"); 
+    // end highlighting
     
     // events
     $( "#play" ).click( onClick );
